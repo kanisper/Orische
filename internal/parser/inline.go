f899199b58280d782bea9d63b72a8431676eb6d6
Kentaro Inui <kentaro.techtech@gmail.com>
2026-07-14 17:39:56 +0900

    Implement inline parser

---
+++ internal/parser/inline.go
@@ -0,0 +1,147 @@
+package parser
+
+import (
+	"strings"
+
+	"medoc/internal/ast"
+)
+
+func parseInlines(text string) ([]ast.Inline, error) {
+	nodes, _, _, err := parseInlineSeq(text, 0, false)
+	if err != nil {
+		return nil, err
+	}
+
+	return nodes, nil
+}
+
+// return []ast.Inline  the parsed inline nodes
+// return int           the position after the parsed inline nodes
+// return bool          whether the parsing stopped at a closing brace
+// return error
+func parseInlineSeq(text string, start int, stopAtClosingBrace bool) ([]ast.Inline, int, bool, error) {
+	var nodes []ast.Inline
+	var ctx strings.Builder
+
+	flushCtx := func() {
+		if ctx.Len() == 0 {
+			return
+		}
+		nodes = append(nodes, &ast.Text{Value: ctx.String()})
+		ctx.Reset()
+	}
+
+	pos := start
+
+	for pos < len(text) {
+		if stopAtClosingBrace && text[pos] == '}' {
+			flushCtx()
+			return nodes, pos + 1, true, nil
+		}
+
+		if hasInlinePrefix(text, pos) {
+			node, next, ok, err := parseInline(text, pos)
+			if err != nil {
+				return nil, 0, false, err
+			}
+			if ok {
+				flushCtx()
+				nodes = append(nodes, node)
+				pos = next
+				continue
+			}
+		}
+
+		ctx.WriteByte(text[pos])
+		pos++
+	}
+
+	flushCtx()
+	return nodes, pos, false, nil
+}
+
+func hasInlinePrefix(text string, pos int) bool {
+	return strings.HasPrefix(text[pos:], ":[")
+}
+
+func parseInline(text string, start int) (ast.Inline, int, bool, error) {
+	headerStart := start + 2
+
+	headerEnd := strings.Index(text[headerStart:], "]{")
+	if headerEnd < 0 {
+		return nil, start, false, nil
+	}
+	headerEnd += headerStart
+
+	dirtype, attr, ok := parseInlineHeader(text[headerStart:headerEnd])
+	if !ok {
+		return nil, start, false, nil
+	}
+
+	contentStart := headerEnd + 2
+	if contentStart+1 >= len(text) {
+		return nil, start, false, nil
+	}
+
+	switch dirtype {
+	case "em":
+		content, next, closed, err := parseInlineSeq(text, contentStart, true)
+		if err != nil {
+			return nil, start, false, nil
+		}
+		if !closed {
+			return nil, start, false, nil
+		}
+
+		return &ast.Emphasis{
+			Content: content,
+		}, next, true, nil
+
+	case "link":
+		if attr == "" {
+			return nil, start, false, nil
+		}
+
+		content, next, closed, err := parseInlineSeq(text, contentStart, true)
+		if err != nil {
+			return nil, start, false, err
+		}
+		if !closed {
+			return nil, start, false, nil
+		}
+
+		return &ast.Link{
+			URI:     attr,
+			Content: content,
+		}, next, true, nil
+
+	case "code":
+		contentEnd := strings.IndexByte(text[contentStart:], '}')
+		if contentEnd < 0 {
+			return nil, start, false, nil
+		}
+		contentEnd += contentStart
+
+		return &ast.CodeSpan{
+			Value: text[contentStart:contentEnd],
+		}, contentEnd + 1, true, nil
+
+	default:
+		return nil, start, false, nil
+	}
+}
+
+func parseInlineHeader(header string) (dirtype string, attr string, ok bool) {
+	if sep := strings.IndexByte(header, ':'); sep >= 0 {
+		dirtype = header[:sep]
+		attr = header[sep+1:]
+	} else {
+		dirtype = header
+		attr = ""
+	}
+
+	if dirtype == "" {
+		return "", "", false
+	}
+	return dirtype, attr, true
+}
---
+++ internal/parser/inline_test.go
@@ -0,0 +1,109 @@
+package parser
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+
+	"medoc/internal/ast"
+)
+
+func TestParseInline(t *testing.T) {
+	input := "plain text 1 :[em]{emphasized text} :[code]{codespan text}:[link:http://example.com]{link text}"
+
+	want := []ast.Inline{
+		&ast.Text{
+			Value: "plain text 1 ",
+		},
+		&ast.Emphasis{
+			Content: []ast.Inline{
+				&ast.Text{Value: "emphasized text"},
+			},
+		},
+		&ast.Text{
+			Value: " ",
+		},
+		&ast.CodeSpan{
+			Value: "codespan text",
+		},
+		&ast.Link{
+			URI: "http://example.com",
+			Content: []ast.Inline{
+				&ast.Text{Value: "link text"},
+			},
+		},
+	}
+
+	output, err := parseInlines(input)
+
+	if err != nil {
+		t.Errorf("parse failed.")
+	}
+	if diff := cmp.Diff(want, output); diff != "" {
+		t.Errorf("parsed incorrectly.\n(-want +got)\n%s\n", diff)
+	}
+}
+
+func TestParseNestedInlines(t *testing.T) {
+	input := ":[em]{emphasized text1 :[link:http://example.com]{linktext} emphasize text2}"
+
+	want := []ast.Inline{
+		&ast.Emphasis{
+			Content: []ast.Inline{
+				&ast.Text{Value: "emphasized text1 "},
+				&ast.Link{
+					URI: "http://example.com",
+					Content: []ast.Inline{
+						&ast.Text{Value: "linktext"},
+					},
+				},
+				&ast.Text{Value: " emphasize text2"},
+			},
+		},
+	}
+
+	output, err := parseInlines(input)
+
+	if err != nil {
+		t.Errorf("parse failed.")
+	}
+	if diff := cmp.Diff(want, output); diff != "" {
+		t.Errorf("parsed incorrectly.\n(-want +got)\n%s\n", diff)
+	}
+}
+
+func TestParseNestedCodespan(t *testing.T) {
+	input := ":[code]{codespan :[em]{this directive should be escaped}}"
+	want := []ast.Inline{
+		&ast.CodeSpan{
+			Value: "codespan :[em]{this directive should be escaped",
+		},
+		&ast.Text{Value: "}"},
+	}
+
+	output, err := parseInlines(input)
+
+	if err != nil {
+		t.Errorf("parse failed.")
+	}
+	if diff := cmp.Diff(want, output); diff != "" {
+		t.Errorf("parsed incorrectly.\n(-want +got)\n%s\n", diff)
+	}
+}
+
+func TestParseInvalidInlines(t *testing.T) {
+	input := ":[em]{There is no end brace."
+
+	want := []ast.Inline{
+		&ast.Text{Value: ":[em]{There is no end brace."},
+	}
+
+	output, err := parseInlines(input)
+
+	if err != nil {
+		t.Errorf("parse failed.")
+	}
+	if diff := cmp.Diff(want, output); diff != "" {
+		t.Errorf("parsed incorrectly.\n(-want +got)\n%s\n", diff)
+	}
+}
