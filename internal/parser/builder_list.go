acd20ffe1a7f4fb16b556e3b2262c55da1f8a9b9
Kentaro Inui <kentaro.techtech@gmail.com>
2026-07-16 00:35:22 +0900

    Define and Implement list builder

---
+++ internal/parser/builder_list.go
@@ -0,0 +1,86 @@
+package parser
+
+import (
+	"fmt"
+	"strings"
+
+	"medoc/internal/ast"
+)
+
+type listBuilder struct{}
+
+func (*listBuilder) build(node parsedBlockNode) (ast.Block, error) {
+	list, ok := node.(*parsedList)
+	if !ok {
+		return nil, fmt.Errorf("expected *parsedList, got %T", node)
+	}
+
+	return buildList(list)
+}
+
+func buildList(pl *parsedList) (*ast.List, error) {
+	list := &ast.List{
+		Ordered: pl.Ordered,
+		Items:   make([]*ast.ListItem, 0, len(pl.Items)),
+		Range:   pl.Range,
+	}
+
+	for _, parsedItem := range pl.Items {
+		item, err := buildListItem(parsedItem)
+		if err != nil {
+			return nil, err
+		}
+
+		list.Items = append(list.Items, item)
+	}
+
+	return list, nil
+}
+
+func buildListItem(parsedItem parsedListItem) (*ast.ListItem, error) {
+	blocks := make([]ast.Block, 0, len(parsedItem.Blocks))
+
+	for _, node := range parsedItem.Blocks {
+		block, err := buildListItemBlock(node)
+		if err != nil {
+			return nil, err
+		}
+
+		blocks = append(blocks, block)
+	}
+
+	return &ast.ListItem{
+		Blocks: blocks,
+		Range:  parsedItem.Range,
+	}, nil
+}
+
+func buildListItemBlock(node parsedBlockNode) (ast.Block, error) {
+	switch block := node.(type) {
+	case *parsedBlock:
+		if !strings.EqualFold(block.Type, "paragraph") {
+			return nil, fmt.Errorf("list builder: unexpected block type in list item: %q", block.Type)
+		}
+
+		inlines, err := parseInlines(block.Text)
+		if err != nil {
+			return nil, fmt.Errorf("list builder: parse paragraph inlines: %w", err)
+		}
+
+		return &ast.Paragraph{
+			Content: inlines,
+			Range:   block.Range,
+		}, nil
+
+	case *parsedList:
+		nested, err := buildList(block)
+		if err != nil {
+			return nil, fmt.Errorf("list builder: build nested list: %w", err)
+		}
+
+		return nested, nil
+
+	default:
+		return nil, fmt.Errorf("list builder: unsupported type of node in list item: %T", node)
+	}
+}
---
+++ internal/parser/builder_list_test.go
@@ -0,0 +1,165 @@
+package parser
+
+import (
+	"testing"
+
+	"medoc/internal/ast"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestBuildList(t *testing.T) {
+	input := &parsedList{
+		Ordered: true,
+		Items: []parsedListItem{
+			{
+				Blocks: []parsedBlockNode{
+					&parsedBlock{
+						Type: "Paragraph",
+						Attr: "",
+						Text: "ol level 1 line 1",
+						Range: ast.Range{
+							StartLine: 1,
+							EndLine:   1,
+						},
+					},
+				},
+				Range: ast.Range{
+					StartLine: 1,
+					EndLine:   1,
+				},
+			},
+			{
+				Blocks: []parsedBlockNode{
+					&parsedBlock{
+						Type: "Paragraph",
+						Attr: "",
+						Text: "ol level 1 line 2",
+						Range: ast.Range{
+							StartLine: 2,
+							EndLine:   2,
+						},
+					},
+					&parsedList{
+						Ordered: false,
+						Items: []parsedListItem{
+							{
+								Blocks: []parsedBlockNode{
+									&parsedBlock{
+										Type: "Paragraph",
+										Attr: "",
+										Text: "ul level 2 line 1",
+										Range: ast.Range{
+											StartLine: 3,
+											EndLine:   3,
+										},
+									},
+								},
+								Range: ast.Range{
+									StartLine: 3,
+									EndLine:   3,
+								},
+							},
+						},
+						Range: ast.Range{
+							StartLine: 3,
+							EndLine:   3,
+						},
+					},
+				},
+				Range: ast.Range{
+					StartLine: 2,
+					EndLine:   3,
+				},
+			},
+			{
+				Blocks: []parsedBlockNode{
+					&parsedBlock{
+						Type: "Paragraph",
+						Attr: "",
+						Text: "ol level 1 line 3",
+						Range: ast.Range{
+							StartLine: 4,
+							EndLine:   4,
+						},
+					},
+				},
+				Range: ast.Range{
+					StartLine: 4,
+					EndLine:   4,
+				},
+			},
+		},
+		Range: ast.Range{
+			StartLine: 1,
+			EndLine:   4,
+		},
+	}
+
+	want := &ast.List{
+		Ordered: true,
+		Items: []*ast.ListItem{
+			{
+				Blocks: []ast.Block{
+					&ast.Paragraph{
+						Content: []ast.Inline{
+							&ast.Text{Value: "ol level 1 line 1"},
+						},
+						Range: ast.Range{StartLine: 1, EndLine: 1},
+					},
+				},
+				Range: ast.Range{StartLine: 1, EndLine: 1},
+			},
+			{
+				Blocks: []ast.Block{
+					&ast.Paragraph{
+						Content: []ast.Inline{
+							&ast.Text{Value: "ol level 1 line 2"},
+						},
+						Range: ast.Range{StartLine: 2, EndLine: 2},
+					},
+					&ast.List{
+						Ordered: false,
+						Items: []*ast.ListItem{
+							{
+								Blocks: []ast.Block{
+									&ast.Paragraph{
+										Content: []ast.Inline{
+											&ast.Text{Value: "ul level 2 line 1"},
+										},
+										Range: ast.Range{StartLine: 3, EndLine: 3},
+									},
+								},
+								Range: ast.Range{StartLine: 3, EndLine: 3},
+							},
+						},
+						Range: ast.Range{StartLine: 3, EndLine: 3},
+					},
+				},
+				Range: ast.Range{StartLine: 2, EndLine: 3},
+			},
+			{
+				Blocks: []ast.Block{
+					&ast.Paragraph{
+						Content: []ast.Inline{
+							&ast.Text{Value: "ol level 1 line 3"},
+						},
+						Range: ast.Range{StartLine: 4, EndLine: 4},
+					},
+				},
+				Range: ast.Range{StartLine: 4, EndLine: 4},
+			},
+		},
+		Range: ast.Range{StartLine: 1, EndLine: 4},
+	}
+
+	got, err := (&listBuilder{}).build(input)
+
+	if err != nil {
+		t.Errorf("build failed: %v", err)
+	}
+
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("build incorrectly.\n(-want +got)\n%s", diff)
+	}
+}
